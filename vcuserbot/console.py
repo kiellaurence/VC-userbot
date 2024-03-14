c468fd4559619de9ca037c1382b6a83592c91375
K <43465633+kiellaurence@users.noreply.github.com>
2024-03-14 22:06:41 +0700

    Update console.py

--- vcuserbot/console.py
+++ vcuserbot/console.py
@@ -33,7 +33,7 @@ API_HASH = getenv("API_HASH", None)
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING_SESSION = getenv("STRING_SESSION", None)
 MONGO_DB_URL = getenv("MONGO_DB_URL", None)
-SUDOERS = getenv ("SUDOERS", "1253258650")
+SUDOERS = getenv ("SUDOERS", "6965945364")
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", 0))
 
 
@@ -46,7 +46,7 @@ COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", ". !").split())
 # OTHERS VARIABLES
 
 # PM GUARD VARS
-PM_GUARD = bool(getenv("PM_GUARD", True))
+PM_GUARD = bool(getenv("PM_GUARD", False))
 PM_GUARD_TEXT = getenv("PM_GUARD_TEXT", "**🥀 Hey, I am an advanced & superfast high quality userbot assistant with an upgraded version security system.\n\n🌿 I can't let you message my owner's dm without my owner's permission.\n\n🌺 My owner is offline now, please wait until my owner allows you.\n\n🍂 Please don't spam here, because spamming will force me to block you from my owner id.**")
 PM_GUARD_LIMIT = int(getenv("PM_GUARD_LIMIT", 5))
 
@@ -66,7 +66,7 @@ FLOODXD = {}
 OLD_MSG = {}
 PM_LIMIT = {}
 PLUGINS = {}
-SUDOERS = [1253258650]
+SUDOERS = [6965945364]
 
 
 COMMAND_HANDLERS = []
