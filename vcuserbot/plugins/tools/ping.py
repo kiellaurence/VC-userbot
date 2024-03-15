d29de293aeec27741d479b7e58a0e6e49c9ccc35
K <43465633+kiellaurence@users.noreply.github.com>
2024-03-15 13:38:19 +0700

    Update ping.py

--- vcuserbot/plugins/tools/ping.py
+++ vcuserbot/plugins/tools/ping.py
@@ -8,7 +8,7 @@ async def ping(client, message):
     start = datetime.now()
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    await eor(message, f"""<b>Pinged !</b>\n<b>Latency:</b> `{ms}` ms\n<b>On Chat:</b> `{title}`\n</b>Owner:</b> <a href='tg://user?id=6965945364'>k</a>""")
+    await eor(message, f"""<b>Pinged !</b>\n<b>Latency:</b> `{ms}` ms\n<b>On Chat:</b> `{title}`\n<b>Owner:</b> <a href='tg://user?id=6965945364'>k</a>""")
 
 
 
