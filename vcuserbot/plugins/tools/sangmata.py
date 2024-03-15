94a05aa52e5feb1dff9f86f3dfa8d27ae0d1f532
K <43465633+kiellaurence@users.noreply.github.com>
2024-03-15 12:16:02 +0700

    Update sangmata.py

--- vcuserbot/plugins/tools/sangmata.py
+++ vcuserbot/plugins/tools/sangmata.py
@@ -26,7 +26,7 @@ async def sg(client: Client, message: Message):
         try:
             user = await client.get_users(args)
         except Exception:
-            return await lol.edit(message, f"`Please specify a valid user!`")
+            return await lol.edit(f"`Please specify a valid user!`")
     bot = "SangMata_BOT"
     try:
         await client.send_message(bot, f"history {user.id}")
