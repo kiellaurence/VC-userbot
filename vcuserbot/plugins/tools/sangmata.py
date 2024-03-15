23ad7d96b333bd249e88a623ff837ac081364114
K <43465633+kiellaurence@users.noreply.github.com>
2024-03-15 10:59:08 +0700

    Update sangmata.py

--- vcuserbot/plugins/tools/sangmata.py
+++ vcuserbot/plugins/tools/sangmata.py
@@ -21,18 +21,18 @@ from pyrogram.types import *
 @sudo_users_only
 async def sg(client: Client, message: Message):
     args = await extract_user(message)
-    lol = await edit_or_reply(message, "`Processing...`")
+    lol = await eor(message, "`Processing...`")
     if args:
         try:
             user = await client.get_users(args)
         except Exception:
-            return await lol.edit(f"`Please specify a valid user!`")
-    bot = "SangMataInfo_bot"
+            return await lol.reply(f"`Please specify a valid user!`")
+    bot = "SangMata_BOT"
     try:
-        await client.send_message(bot, f"/search_id {user.id}")
+        await client.send_message(bot, f"history {user.id}")
     except YouBlockedUser:
         await client.unblock_user(bot)
-        await client.send_message(bot, f"/search_id {user.id}")
+        await client.send_message(bot, f"history {user.id}")
     await asyncio.sleep(1)
 
     async for stalk in client.search_messages(bot, query="Name", limit=1):
