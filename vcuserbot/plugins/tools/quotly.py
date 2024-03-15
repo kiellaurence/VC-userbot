bb57206e67da7591b5870a623eb0fbf410e8a8bb
K <43465633+kiellaurence@users.noreply.github.com>
2024-03-16 03:13:41 +0700

    Update quotly.py

--- vcuserbot/plugins/tools/quotly.py
+++ vcuserbot/plugins/tools/quotly.py
@@ -12,7 +12,7 @@ from ... import *
 
 @app.on_message(cdx("q"))
 @sudo_users_only
-async def quotly(client, message):
+async def quotly(client: Client, message: Message):
     if not message.reply_to_message:
         await message.edit("Reply to any users text message")
         return
