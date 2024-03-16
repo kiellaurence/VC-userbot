c2d8c12c736270b69c1f3e53a72ddad07124ebd9
K <43465633+kiellaurence@users.noreply.github.com>
2024-03-16 10:50:02 +0700

    Update events.py

--- vcuserbot/modules/helpers/events.py
+++ vcuserbot/modules/helpers/events.py
@@ -20,6 +20,15 @@ async def edit_or_reply(message: Message, *args, **kwargs) -> Message:
     
     return await msg(*args, **kwargs)
 
+async def edit_or_replyy(message: Message, *args, **kwargs) -> Message:
+    apa = (
+        message.edit_text
+        if bool(message.from_user and message.from_user.is_self or message.outgoing)
+        else (message.reply_to_message or message).reply_text
+    )
+    return await apa(*args, **kwargs)
+
+
 async def extract_userid(message, text: str):
     def is_int(text: str):
         try:
