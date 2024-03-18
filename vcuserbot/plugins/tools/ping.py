da6aaf1b2728fc97c344973f4360dac0e9eebd61
K <43465633+kiellaurence@users.noreply.github.com>
2024-03-18 15:24:17 +0700

    Update ping.py

--- vcuserbot/plugins/tools/ping.py
+++ vcuserbot/plugins/tools/ping.py
@@ -1,14 +1,18 @@
 from ... import *
+import requests
+import time
 from datetime import datetime
 
 @app.on_message(cdx("ping"))
 @sudo_users_only
 async def ping(client, message):
+    response = requests.get('https://www.google.com')
+    elapsed_time = response.elapsed
     title = message.chat.title
     start = datetime.now()
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    await eor(message, f"""<b>Pinged !</b>\n<b>Latency:</b> `{ms}` ms\n<b>On Chat:</b> `{title}`\n<b>Owner:</b> <a href='tg://user?id=6965945364'>k</a>""")
+    await eor(message, f"""<b>Pinged !</b>\n<b>Latency:</b> `{elapsed_time}` ms\n<b>On Chat:</b> `{title}`\n<b>Owner:</b> <a href='tg://user?id=6965945364'>k</a>""")
 
 
 
