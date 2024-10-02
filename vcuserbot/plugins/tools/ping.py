e2fb848f131ba6e4d092e705f8828e4724bba4d5
K <43465633+kiellaurence@users.noreply.github.com>
2024-10-02 21:36:44 +0700

    Update ping.py

--- vcuserbot/plugins/tools/ping.py
+++ vcuserbot/plugins/tools/ping.py
@@ -6,6 +6,7 @@ from datetime import datetime
 @app.on_message(cdx("ping"))
 @sudo_users_only
 async def ping(client, message):
+    title = message.chat.title
     start = datetime.now()
     end = datetime.now()
     ms = (end - start).microseconds / 1000
